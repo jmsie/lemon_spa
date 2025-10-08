b5eee5022bd913d6bd422645429d10e95f2ebce1
Jeff Lu <jeff@gogoout.com>
2025-10-08 12:15:29 +0800

    landing page

--- www/lemon_spa/urls.py
+++ www/lemon_spa/urls.py
@@ -2,8 +2,10 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="landing.html"), name="landing"),
     path("admin/", admin.site.urls),
     path("api/", include("lemon_spa.api.urls", namespace="api")),
     path("accounts/", include("accounts.urls", namespace="accounts")),
